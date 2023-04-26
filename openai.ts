2c7c72f21b940b50353e3220ba967c64eb40025c
ryanbakkerNZ <ryan@ryanbakker.nz>
2023-04-26 22:12:14 +1200

    Get ChatGPT Suggestion

--- openai.ts
+++ openai.ts
@@ -2,7 +2,7 @@ import { Configuration, OpenAIApi } from "openai";
 
 const config = new Configuration({
   organization: process.env.OPEN_AI_ORGANIZATION,
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPEN_AI_KEY,
 });
 
 const openai = new OpenAIApi(config);
